b18faa6f24ccf20827dfab28375a4cbfae508f4a
programminglaboratorys <programming.laboratorys@gmail.com>
2024-06-21 15:58:09 +0300

    support cloning icons

--- src/plugins/roleDuplication/index.tsx
+++ src/plugins/roleDuplication/index.tsx
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+import "./style.css";
+
 import { NavContextMenuPatchCallback } from "@api/ContextMenu";
 import definePlugin from "@utils/types";
 import { GuildStore, Menu, SelectedGuildStore } from "@webpack/common";
@@ -12,14 +14,13 @@ import { Guild, Role } from "discord-types/general";
 import { createRole } from "./api";
 import { openModal } from "./modal";
 
-
 function MakeContextCallback(type: "settings" | "other"): NavContextMenuPatchCallback {
     return type === "settings" ? (children, { guild, role }: { guild: Guild; role: Role; }) => {
         children.splice(-1, 0,
             <Menu.MenuItem
                 id={"vc-dup-role"}
                 label="Duplicate"
-                action={async () => { createRole(guild, role); }}
+                action={async () => { createRole(guild, role, role.icon ? `https://cdn.discordapp.com/role-icons/${role.id}/${role.icon}.webp` : null); }}
             />
         );
     } : (children, contextMenuApiArguments) => {
@@ -30,7 +31,7 @@ function MakeContextCallback(type: "settings" | "other"): NavContextMenuPatchCal
             <Menu.MenuItem
                 id={"vc-dup-role"}
                 label="Clone"
-                action={() => openModal(role)}
+                action={() => openModal(role, role.icon ? `https://cdn.discordapp.com/role-icons/${role.id}/${role.icon}.webp` : null)}
             />
         );
     };
